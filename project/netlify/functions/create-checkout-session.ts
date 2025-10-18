57d131490fd4461159038ca3c166c51d23b14d7f
Thomas <boyer_thomas@hotmail.fr>
2025-10-18 21:27:16 +0200

    d

--- project/netlify/functions/create-checkout-session.ts
+++ project/netlify/functions/create-checkout-session.ts
@@ -2,7 +2,7 @@ import { Handler } from '@netlify/functions';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2024-10-28.acacia',
+  apiVersion: '2025-09-30.clover',
 });
 
 export const handler: Handler = async (event) => {
